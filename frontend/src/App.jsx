cd12d3f8d0734bbffc3324c8122c58b86b57e26b
UjjwalTyagi-2749 <gamingcyber805@gmail.com>
2026-07-14 19:14:58 +0530

    donr

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,26 @@
+import { Routes, Route } from "react-router-dom";
+
+import Dashboard from "./pages/Dashboard.jsx";
+import Watchlist from "./pages/Watchlist.jsx";
+import Applications from "./pages/Applications.jsx";
+import Preparation from "./pages/Preparation.jsx";
+import Notifications from "./pages/Notifications.jsx";
+import Profile from "./pages/Profile.jsx";
+import Login from "./pages/Login.jsx";
+import Register from "./pages/Register.jsx";
+
 function App() {
   return (
-    <div>
-      <h1>InternPilot</h1>
-    </div>
+    <Routes>
+      <Route path="/" element={<Dashboard />} />
+      <Route path="/watchlist" element={<Watchlist />} />
+      <Route path="/applications" element={<Applications />} />
+      <Route path="/preparation" element={<Preparation />} />
+      <Route path="/notifications" element={<Notifications />} />
+      <Route path="/profile" element={<Profile />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+    </Routes>
   );
 }
 
---
+++ frontend/src/pages/Watchlist.jsx
@@ -0,0 +1,6 @@
+
+function Watchlist() {
+  return <h1>Watchlist</h1>;
+}
+
+export default Watchlist;
